467b251f83fde3969234fab57885cd803260f2f3
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-09-12 10:42:27 +0900

    [feat] remember, rememberSaveable 사용

--- codelab/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ codelab/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -1,9 +1,16 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
@@ -14,6 +21,12 @@ fun WellnessTaskItem(
     Row(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
-
+        Text(
+            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
     }
 }
